e1fcd046929c4de2927bd445657dc19cb0460cf8
Patrick GIRY <patrickgiry@gmail.com>
2017-12-11 14:54:12 +0100

    5 should have one prime factor 5

--- prime.factors.spec.js
+++ prime.factors.spec.js
@@ -18,4 +18,8 @@ describe('PrimeFactors', () => {
     it ('4 should have two prime factors 2 and 2', () => {
         expect(primeFactorsOf(4)).toEqual([2, 2])
     })
+
+    it ('5 should have one prime factor 5', () => {
+        expect(primeFactorsOf(5)).toEqual([5])
+    })
 })
