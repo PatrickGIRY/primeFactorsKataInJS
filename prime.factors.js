c1645c1e0f06d2e95508e5127e0ad628db3edd6f
Patrick GIRY <patrickgiry@gmail.com>
2017-12-11 15:30:44 +0100

    refactor transform first while to for loop

--- prime.factors.js
+++ prime.factors.js
@@ -2,12 +2,10 @@
 
 const primeFactorsOf = n => {
     let factors = []
-    let divisor = 2;
-    while (n > 1) {
+    for (let divisor = 2; n > 1; divisor++) {
         for (;n % divisor === 0; n /= divisor) {
             factors.push(divisor)
         }
-        divisor++
     }
     return factors
 }
